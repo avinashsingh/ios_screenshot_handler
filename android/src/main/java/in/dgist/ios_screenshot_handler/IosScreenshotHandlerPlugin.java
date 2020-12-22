6339cd2f3dbd05c5046cd2d81a5149508d7581e6
Avinash Singh <avinash@dgist.in>
2020-12-22 15:22:16 +0530

    screen count

--- android/src/main/java/in/dgist/ios_screenshot_handler/IosScreenshotHandlerPlugin.java
+++ android/src/main/java/in/dgist/ios_screenshot_handler/IosScreenshotHandlerPlugin.java
@@ -1,23 +1,36 @@
 package in.dgist.ios_screenshot_handler;
 
+import android.app.Activity;
+
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
+import android.hardware.display.DisplayManager;
+
+
 /** IosScreenshotHandlerPlugin */
 public class IosScreenshotHandlerPlugin implements MethodCallHandler {
+  private final Activity activity;
   /** Plugin registration. */
   public static void registerWith(Registrar registrar) {
     final MethodChannel channel = new MethodChannel(registrar.messenger(), "ios_screenshot_handler");
-    channel.setMethodCallHandler(new IosScreenshotHandlerPlugin());
+    channel.setMethodCallHandler(new IosScreenshotHandlerPlugin(registrar.activity()));
+  }
+
+  private IosScreenshotHandlerPlugin(Activity activity) {
+    this.activity = activity;
   }
 
   @Override
   public void onMethodCall(MethodCall call, Result result) {
     if (call.method.equals("getPlatformVersion")) {
       result.success("Android " + android.os.Build.VERSION.RELEASE);
+    } else if (call.method.equals("displayCount")) {
+      int displayLength = ((DisplayManager)this.activity.getSystemService(this.activity.DISPLAY_SERVICE)).getDisplays().length;
+      result.success(displayLength);
     } else {
       result.notImplemented();
     }
--- lib/ios_screenshot_handler.dart
+++ lib/ios_screenshot_handler.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:core';
 
 import 'package:flutter/services.dart';
+import 'dart:io';
 
 typedef MessageHandler = Future<void> Function();
 
@@ -15,12 +16,15 @@ class IosScreenshotHandler {
   }
 
   MessageHandler onScreenshotL;
+  MessageHandler onMultipleDisplaysL;
 
-  configure (MessageHandler onScreenshot) {
+  configure (MessageHandler onScreenshot, MessageHandler onMultipleDisplays) {
     this.onScreenshotL = onScreenshot;
+    this.onMultipleDisplaysL = onMultipleDisplays;
     _channel.setMethodCallHandler(_handleMethod);
     Timer.periodic(Duration(seconds: 1), (_timer) {
       recording();
+      displaysCount();
     });
   }
 
@@ -29,12 +33,26 @@ class IosScreenshotHandler {
   }
 
   Future<bool> recording() async {
-    final bool recording = await _channel.invokeMethod('recording');
-    if (recording) {
-      print("Recording continues ${recording}");
-      onScreenshotL();
+    if (Platform.isIOS) {
+      final bool recording = await _channel.invokeMethod('recording');
+      if (recording) {
+        print("Recording continues ${recording}");
+        onScreenshotL();
+      }
+      return recording;
     }
-    return recording;
+    return false;
+  }
+
+  Future<int> displaysCount() async {
+    if(Platform.isIOS) {
+      return 1;
+    }
+    final int count = await _channel.invokeMethod('displayCount');
+    if (count > 1) {
+      onMultipleDisplaysL();
+    }
+    return count;
   }
 
 }
